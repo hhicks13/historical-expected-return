c3bdce18cd0bc75c82d830af46dfd12e5c95fefd
Harrison Hicks <hhicks13@vols.utk.edu>
2021-07-13 16:38:15 +0000

    functioning

--- sample-compute-pickle.py
+++ sample-compute-pickle.py
@@ -2,30 +2,49 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import yfinance as yf
+import sys
 
 #
 # calibrate timing
 #
 _begin = '2021-03-11'
 _end = datetime.now()
-_td = pd.Timestamp(_begin) - pd.Timestamp(_end)
-_period = str(-_td.days)+'d'
-_interval = '1h'
-print(_period)
+#_td = pd.Timestamp(_begin) - pd.Timestamp(_end)
+#_period = str(-_td.days)+'d'
+#_interval = '1h'
+
 #
 # get btc and eth median spot prices
 #
-data_eth = yf.download(tickers='ETH-USD',period=_period,interval=_interval)
-data_btc = yf.download(tickers='BTC-USD',period=_period,interval=_interval)
+data_eth = yf.download(tickers='ETH-USD',start=pd.Timestamp(_begin),end=pd.Timestamp(_end),interval='1h')
+data_btc = yf.download(tickers='BTC-USD',start=pd.Timestamp(_begin),end=pd.Timestamp(_end),interval='1h')
 _btc = pd.Series(data=data_btc["Close"])
 _eth = pd.Series(data=data_eth["Close"])
 #
-_btc_resampled = _btc.resample('6H').median()
-_eth_resampled = _eth.resample('6H').median()
+_btc_resampled = _btc.resample('6H').mean()
+_eth_resampled = _eth.resample('6H').mean()
 # get dict
-btc_spot_prices = _btc_resampled.to_dict()
-eth_spot_prices = _eth_resampled.to_dict()
+_bsp0 = _btc_resampled.to_dict()
+_esp0 = _eth_resampled.to_dict()
 
+    
+#
+# clean timestamp keys
+#
+btc_spot_prices = {}
+eth_spot_prices = {}
+for _key in _bsp0.keys():
+    val_btc = _bsp0[_key]
+    val_eth = _esp0[_key]
+    print(val_btc)
+    print(val_eth)
+    
+    _key = _key.replace(second=0,microsecond=0,minute=0)
+    key = pd.Timestamp(year=_key.year,month=_key.month,day=_key.day,hour=_key.hour,minute=0,second=0)
+    
+    btc_spot_prices[key] = val_btc
+    eth_spot_prices[key] = val_eth
+    
 #
 # define methods
 #
@@ -35,22 +54,29 @@ def _asset(sym):
     return asset
 
 def _spot_price(asset,T):
+    print(type(T))
+    print(T)
     if asset == "BTC":
-        return btc_spot_prices(T)
+        val = btc_spot_prices[T]
+        return val
     elif asset == "ETH":
-        return eth_spot_prices(T)
+        val = eth_spot_prices[T]
+        return val
     else: raise ValueError("asset type invalid, must be ETH or BTC")
 
 def _exercised(sym):
     tokens = sym.split('-')
     asset = tokens[0]
-    T = datetime.datetime.strptime(tokens[1],'%d%b%y')
+    T = pd.Timestamp(datetime.strptime(tokens[1],'%d%b%y'))
+    if T < datetime.now():return 0
+    print("type going into exercised",type(T))
     K = int(tokens[2])
     Y = int(tokens[3] == 'C')
+    S = lambda asset,T : btc_spot_prices[T] if asset == 'BTC' else eth_spot_prices[T]
     if Y == 1:
-        return int(max(spot_price(asset,T)-K,0) > 0)
+        return int(max(S(asset,T)-K,0) > 0)
     elif Y == 0:
-        return int(max(K-spot_price(asset,T),0) > 0)
+        return int(max(K-S(asset,T),0) > 0)
     else:
         raise ValueError("Y invalid, must be 1 or 0")
 
@@ -64,7 +90,7 @@ def _strike(sym):
 
 def _maturity(sym):
     tokens = sym.split('-')
-    return datetime.datetime.strptime(tokens[1],'%d%b%y')
+    return datetime.strptime(tokens[1],'%d%b%y')
 #
 #
 #
@@ -78,7 +104,9 @@ _chi = np.vectorize(_exercised)
 #
 def _S(syms,t):
     A = _A(syms)
-    S = _spot_price(A,t)
+    spot = lambda asset : btc_spot_prices[t] if asset == 'BTC' else eth_spot_prices[t]
+    _spot = np.vectorize(spot)
+    S = _spot(A)
     return np.column_stack((S,np.ones(len(A))))
 
 def _rmin_loss(syms):
@@ -102,6 +130,7 @@ def _Rprime(p,syms):
 def instantaneous_return(syms,p,t):
     R,_ = _rmin_loss(syms)
     Rprime = _Rprime(p,syms)
+    # need to return vector of spot prices
     S = _S(syms,t)
     TVR = np.matmul(S.transpose(),R).trace()
     TVRp = np.matmul(S.transpose(),Rprime).trace()
@@ -111,8 +140,8 @@ def instantaneous_return(syms,p,t):
 # need to reset data if beginning of new day
 # 
 #
-nrows = 8437280
-directory = "../../options_csv/"
+nrows = 843
+directory = "../options_csv/"
 day = '2021-03-11'
 filename = "deribit_quotes_"+day+"_OPTIONS.csv"
 data = pd.read_csv(directory+ filename,nrows=nrows)
@@ -122,15 +151,20 @@ data = data.set_index("datetime")
 #
 #
 #
-_tg = _btc_resampled.index
-print("min",_tg.min())
-print("max",_tg.max())
-for i,t in enumerate(_tg[:80]):
-    # get timestamp objects
-    ts_start=t.floor('h')
+
+_tg = np.array([key for key in btc_spot_prices.keys()],dtype=object)
+for ts in _tg:
+    print(ts,btc_spot_prices[ts])
+
+for i,_ts in enumerate(_tg[:80]):
+
+    #
+    ts_start = _ts
+    ts_end = _tg[i+1]
+    # convert to string
     str_start = ts_start.strftime('%H:%M')
-    ts_end=_tg[i+1].floor('h')
     str_end = ts_end.strftime('%H:%M')
+    print("type going into main loop",type(ts_start))
     print("median btc spot price at time ",ts_start," is ",btc_spot_prices[ts_start])
     print("median eth spot price at time ",ts_start," is ",eth_spot_prices[ts_start])
     #
@@ -138,9 +172,27 @@ for i,t in enumerate(_tg[:80]):
     #
     deribit_sample = data.between_time(str_start,str_end)
     option_sample = deribit_sample.groupby(['symbol']).median()
+    
     syms = option_sample.index.values
-    p = sample.ask_price.values
-    R = instantaneous_return(syms,p,t)
-    print(R)
+    p = option_sample.ask_price.values
+
+    # remove rows with future expiration (missing data)
+    print("before")
+    print(syms.shape)
+    print(p.shape)
+    T = _T(syms)
+    mask = np.array([t < datetime.now() for t in T],dtype=bool)
+    p = p[mask,...]
+    syms= syms[mask,...]
+    print("after")
+    print(syms.shape)
+    print(p.shape)
+
+    # if empty skip, else compute return
+    if syms.shape[0] == 0:
+        continue
+    else:
+        ir = instantaneous_return(syms,p,ts_start)
+        print(ts_start,ir)
     #
     # if it passes test, then save as pickle object
