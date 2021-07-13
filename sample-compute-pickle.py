4ec9f8a2783fb38211220b4cf4589d1ac398fb7e
Harrison Hicks <hhicks13@vols.utk.edu>
2021-07-13 19:16:41 +0000

    return working

--- sample-compute-pickle.py
+++ sample-compute-pickle.py
@@ -19,30 +19,64 @@ _end = datetime.now()
 data_eth = yf.download(tickers='ETH-USD',start=pd.Timestamp(_begin),end=pd.Timestamp(_end),interval='1h')
 data_btc = yf.download(tickers='BTC-USD',start=pd.Timestamp(_begin),end=pd.Timestamp(_end),interval='1h')
 _btc = pd.Series(data=data_btc["Close"])
+_btc_hi = pd.Series(data=data_btc["High"])
+_btc_low = pd.Series(data=data_btc["Low"])
+
 _eth = pd.Series(data=data_eth["Close"])
+_eth_hi = pd.Series(data=data_eth["High"])
+_eth_low = pd.Series(data=data_eth["Low"])
+
 #
 _btc_resampled = _btc.resample('6H').mean()
 _eth_resampled = _eth.resample('6H').mean()
+
+_btc_hi = _btc_hi.resample('1D').max()
+_eth_hi = _eth_hi.resample('1D').max()
+_btc_low = _btc_low.resample('1D').min()
+_eth_low = _eth_low.resample('1D').min()
+
 # get dict
 _bsp0 = _btc_resampled.to_dict()
 _esp0 = _eth_resampled.to_dict()
 
-    
+_bh0 = _btc_hi.to_dict()
+_bl0 = _btc_low.to_dict()
+_eh0 = _eth_hi.to_dict()
+_el0 = _eth_low.to_dict()
+
+#
 #
 # clean timestamp keys
 #
 btc_spot_prices = {}
 eth_spot_prices = {}
+btc_lh = {}
+eth_lh = {}
+for _key in _bh0.keys():
+    val_bh = _bh0[_key]
+    val_bl = _bl0[_key]
+    val_eh = _eh0[_key]
+    val_el = _el0[_key]
+    key = pd.Timestamp(year=_key.year,month=_key.month,day=_key.day,hour=_key.hour,minute=0,second=0,microsecond=0)
+    btc_lh[key] = (val_bl,val_bh)
+    eth_lh[key] = (val_el,val_eh)
+
 for _key in _bsp0.keys():
     val_btc = _bsp0[_key]
     val_eth = _esp0[_key]
     
-    _key = _key.replace(second=0,microsecond=0,minute=0)
-    key = pd.Timestamp(year=_key.year,month=_key.month,day=_key.day,hour=_key.hour,minute=0,second=0)
+    key = pd.Timestamp(year=_key.year,month=_key.month,day=_key.day,hour=_key.hour,minute=0,second=0,microsecond=0)
+    day = key.replace(hour=0)
+
+    val_bhl = btc_lh[day]
+    val_ehl = eth_lh[day]
     
     btc_spot_prices[key] = val_btc
     eth_spot_prices[key] = val_eth
-    
+
+    btc_lh[key] = val_bhl
+    eth_lh[key] = val_ehl
+
 #
 # define methods
 #
@@ -51,28 +85,18 @@ def _asset(sym):
     asset = tokens[0]
     return asset
 
-def _spot_price(asset,T):
-    if asset == "BTC":
-        val = btc_spot_prices[T]
-        return val
-    elif asset == "ETH":
-        val = eth_spot_prices[T]
-        return val
-    else: raise ValueError("asset type invalid, must be ETH or BTC")
-
 def _exercised(sym):
     tokens = sym.split('-')
     asset = tokens[0]
     T = pd.Timestamp(datetime.strptime(tokens[1],'%d%b%y'))
-    if T < datetime.now():return 0
-
     K = int(tokens[2])
     Y = int(tokens[3] == 'C')
-    S = lambda asset,T : btc_spot_prices[T] if asset == 'BTC' else eth_spot_prices[T]
+    S_max = lambda asset,T : btc_lh[T][1] if asset == 'BTC' else eth_lh[T][1]
+    S_min = lambda asset,T : btc_lh[T][0] if asset == 'BTC' else eth_lh[T][0]
     if Y == 1:
-        return int(max(S(asset,T)-K,0) > 0)
+        return int(max(S_max(asset,T)-K,0) > 0)
     elif Y == 0:
-        return int(max(K-S(asset,T),0) > 0)
+        return int(max(K-S_min(asset,T),0) > 0)
     else:
         raise ValueError("Y invalid, must be 1 or 0")
 
@@ -98,39 +122,50 @@ _chi = np.vectorize(_exercised)
 #
 # need to apply lambda here
 #
-def _S(syms,t):
+def btc2usd_S(p,t):
+    spot = lambda premium : btc_spot_prices[t]*premium
+    _spot = np.vectorize(spot)
+    return _spot(p)
+    
+
+def _Smatrix(syms,t):
     A = _A(syms)
     spot = lambda asset : btc_spot_prices[t] if asset == 'BTC' else eth_spot_prices[t]
     _spot = np.vectorize(spot)
     S = _spot(A)
+    #left column S, right column 1s
     return np.column_stack((S,np.ones(len(A))))
 
 def _rmin_loss(syms):
     Y = _Y(syms)
-    T = _T(syms)
     K = _K(syms)
+    chi = _chi(syms)
     Rmin = np.column_stack((Y,np.multiply(1-Y,K)))
-    chi = np.outer(_chi(syms),np.ones(2))
+    chi2 = np.outer(chi,np.ones(2))
     rhs = np.column_stack((np.ones(len(syms)),K)) - 2*Rmin
-    return Rmin, np.multiply(chi,rhs)
+    return Rmin, np.multiply(chi2,rhs)
 
-def _gain(p):
-    return np.column_stack((np.zeros(len(p)),p))
+def usd_gain(p,t):
+    # convert btc to usd and then reshape matrix.
+    _p = btc2usd_S(p,t)
+    assert _p.shape == p.shape
+    return np.column_stack((np.zeros(len(_p)),_p))
 
-def _Rprime(p,syms):
-    G = _gain(p)
+def _Rprime(p,syms,t):
+    G = usd_gain(p,t)
     Rmin,L = _rmin_loss(syms)
     Rprime = Rmin + L + G
     return Rprime
 
 def instantaneous_return(syms,p,t):
-    R,_ = _rmin_loss(syms)
-    Rprime = _Rprime(p,syms)
-    # need to return vector of spot prices
-    S = _S(syms,t)
+    R,L = _rmin_loss(syms)
+    Rprime = _Rprime(p,syms,t)
+    # need to return matrix of spot prices
+    S = _Smatrix(syms,t)
     TVR = np.matmul(S.transpose(),R).trace()
     TVRp = np.matmul(S.transpose(),Rprime).trace()
-    return TVRp/TVR
+    X = sum([int(l[0] == l[1] == 0) for l in L])/len(L[:,0])
+    return TVRp/TVR,X
 
 # outer loop
 directory = "../options_csv/"
@@ -142,7 +177,7 @@ for i in range(1):
     # 
     #
     #nrows = 8437280
-    nrows = 10
+    nrows = 8437280
     day = '2021-03-11'
     filename = "deribit_quotes_"+day+"_OPTIONS.csv"
     data = pd.read_csv(directory+ filename,nrows=nrows)
@@ -153,7 +188,6 @@ for i in range(1):
     ts_day0 = ts_day0.replace(hour=0,minute=0,second=0)
     ts_day1 = ts_day0.replace(day=ts_day0.day+1)
     _timegrid_mask = np.array([t >= ts_day0 and t < ts_day1 for t in _timegrid],dtype=bool)
-    sys.exit()
     # inner loop #####################################
     for j,_ts in enumerate(_timegrid[_timegrid_mask,...]):
         if _ts.hour == 18:
@@ -186,7 +220,7 @@ for i in range(1):
         if syms.shape[0] == 0:
             continue
         else:
-            ir = instantaneous_return(syms,p,ts_start)
-            print(ts_start,ir)
+            ir,xr = instantaneous_return(syms,p,ts_start)
+            print(ts_start,ir,xr)
             #
             # if it passes test, then save as pickle object
