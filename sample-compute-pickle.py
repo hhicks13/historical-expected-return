3d63fb1afa4fe92a7ecf26949b51be0765c86096
Harrison Hicks <hhicks13@vols.utk.edu>
2021-07-13 17:32:58 +0000

    outer loop

--- sample-compute-pickle.py
+++ sample-compute-pickle.py
@@ -36,8 +36,6 @@ eth_spot_prices = {}
 for _key in _bsp0.keys():
     val_btc = _bsp0[_key]
     val_eth = _esp0[_key]
-    print(val_btc)
-    print(val_eth)
     
     _key = _key.replace(second=0,microsecond=0,minute=0)
     key = pd.Timestamp(year=_key.year,month=_key.month,day=_key.day,hour=_key.hour,minute=0,second=0)
@@ -54,8 +52,6 @@ def _asset(sym):
     return asset
 
 def _spot_price(asset,T):
-    print(type(T))
-    print(T)
     if asset == "BTC":
         val = btc_spot_prices[T]
         return val
@@ -69,7 +65,7 @@ def _exercised(sym):
     asset = tokens[0]
     T = pd.Timestamp(datetime.strptime(tokens[1],'%d%b%y'))
     if T < datetime.now():return 0
-    print("type going into exercised",type(T))
+
     K = int(tokens[2])
     Y = int(tokens[3] == 'C')
     S = lambda asset,T : btc_spot_prices[T] if asset == 'BTC' else eth_spot_prices[T]
@@ -136,63 +132,61 @@ def instantaneous_return(syms,p,t):
     TVRp = np.matmul(S.transpose(),Rprime).trace()
     return TVRp/TVR
 
-#
-# need to reset data if beginning of new day
-# 
-#
-nrows = 843
+# outer loop
 directory = "../options_csv/"
-day = '2021-03-11'
-filename = "deribit_quotes_"+day+"_OPTIONS.csv"
-data = pd.read_csv(directory+ filename,nrows=nrows)
-data = data.dropna()
-data["datetime"] = pd.to_datetime(data['local_timestamp']*1e3)
-data = data.set_index("datetime")
-#
-#
-#
-
-_tg = np.array([key for key in btc_spot_prices.keys()],dtype=object)
-for ts in _tg:
-    print(ts,btc_spot_prices[ts])
-
-for i,_ts in enumerate(_tg[:80]):
-
+_timegrid = np.array([key for key in btc_spot_prices.keys()],dtype=object)
+# outer loop over days ##########
+for i in range(1):
     #
-    ts_start = _ts
-    ts_end = _tg[i+1]
-    # convert to string
-    str_start = ts_start.strftime('%H:%M')
-    str_end = ts_end.strftime('%H:%M')
-    print("type going into main loop",type(ts_start))
-    print("median btc spot price at time ",ts_start," is ",btc_spot_prices[ts_start])
-    print("median eth spot price at time ",ts_start," is ",eth_spot_prices[ts_start])
+    # need to reset data if beginning of new day
+    # 
     #
-    #
-    #
-    deribit_sample = data.between_time(str_start,str_end)
-    option_sample = deribit_sample.groupby(['symbol']).median()
-    
-    syms = option_sample.index.values
-    p = option_sample.ask_price.values
-
-    # remove rows with future expiration (missing data)
-    print("before")
-    print(syms.shape)
-    print(p.shape)
-    T = _T(syms)
-    mask = np.array([t < datetime.now() for t in T],dtype=bool)
-    p = p[mask,...]
-    syms= syms[mask,...]
-    print("after")
-    print(syms.shape)
-    print(p.shape)
-
-    # if empty skip, else compute return
-    if syms.shape[0] == 0:
-        continue
-    else:
-        ir = instantaneous_return(syms,p,ts_start)
-        print(ts_start,ir)
-    #
-    # if it passes test, then save as pickle object
+    #nrows = 8437280
+    nrows = 10
+    day = '2021-03-11'
+    filename = "deribit_quotes_"+day+"_OPTIONS.csv"
+    data = pd.read_csv(directory+ filename,nrows=nrows)
+    data = data.dropna()
+    data["datetime"] = pd.to_datetime(data['local_timestamp']*1e3)
+    data = data.set_index("datetime")
+    ts_day0 = pd.Timestamp(day)
+    ts_day0 = ts_day0.replace(hour=0,minute=0,second=0)
+    ts_day1 = ts_day0.replace(day=ts_day0.day+1)
+    _timegrid_mask = np.array([t >= ts_day0 and t < ts_day1 for t in _timegrid],dtype=bool)
+    sys.exit()
+    # inner loop #####################################
+    for j,_ts in enumerate(_timegrid[_timegrid_mask,...]):
+        if _ts.hour == 18:
+            print("finished sampling day")
+            break
+        #
+        ts_start = _ts
+        ts_end = _timegrid[j+1]
+        # convert to string
+        str_start = ts_start.strftime('%H:%M')
+        str_end = ts_end.strftime('%H:%M')
+        #
+        #
+        deribit_sample = data.between_time(str_start,str_end)
+        option_sample = deribit_sample.groupby(['symbol']).median()
+        syms = option_sample.index.values
+        p = option_sample.ask_price.values
+        # remove rows with future expiration (missing data)
+        print("before")
+        print(syms.shape)
+        print(p.shape)
+        T = _T(syms)
+        mask = np.array([t < datetime.now() for t in T],dtype=bool)
+        p = p[mask,...]
+        syms= syms[mask,...]
+        print("after")
+        print(syms.shape)
+        print(p.shape)
+        # if empty skip, else compute return
+        if syms.shape[0] == 0:
+            continue
+        else:
+            ir = instantaneous_return(syms,p,ts_start)
+            print(ts_start,ir)
+            #
+            # if it passes test, then save as pickle object
